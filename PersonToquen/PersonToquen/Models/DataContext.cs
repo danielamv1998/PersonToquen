6616e1676abf39cbed44d2d8ff51675f8704ce2f
Daniela Melendres Villarreal <a2017113441@estudiantes.upsa.edu.bo>
2021-04-08 19:51:57 -0400

    Se adiciono el controlador people con toquen

---
+++ PersonToquen/PersonToquen/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PersonToquen.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<PersonToquen.Models.Person> People { get; set; }
+    }
+}
---
+++ PersonToquen/PersonToquen/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PersonToquen.Models
+{
+    public class Person
+    {
+        [Key]
+        public int PersonId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int CovidCount { get; set; }
+    }
+}
